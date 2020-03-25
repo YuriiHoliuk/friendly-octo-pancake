cd111068146b10c25c06ec52c52ebd6b1413923a
vladyslav223 <vladyslav223@gmail.com>
2020-03-25 05:02:37 +0200

    change

--- src/components/Content/view/projects/project/index.jsx
+++ src/components/Content/view/projects/project/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 // import './index.scss';
 
 const Project = ({ project }) => (
-  <a href={project.data.homepage} target="_blank" className="project">
+  <a href={project.data.homepage} target="_blank" rel="noopener noreferrer" className="project">
     <div className={`project__container project__name-${project.data.name}`}>
       <div className="project__name">{project.data.description}</div>
       {console.log('props.project', project.data)}
