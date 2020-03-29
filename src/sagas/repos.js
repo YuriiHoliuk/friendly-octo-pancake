219aa8d98ae1945a08cb5ecd0f7b29bd45401558
vladyslav223 <vladyslav223@gmail.com>
2020-03-29 22:02:29 +0300

    link added

--- src/sagas/repos.js
+++ src/sagas/repos.js
@@ -1,5 +1,5 @@
 const repos = [
-  { url: 'https://vladyslav223.github.io/react_uber-eats/' },
+  { url: 'https://api.github.com/repos/Vladyslav223/react_uber-eats' },
   { url: 'https://api.github.com/repos/Vladyslav223/portfolio_layout_dia' },
   { url: 'https://api.github.com/repos/Vladyslav223/portfolio_layout_miami' },
   { url: 'https://api.github.com/repos/Vladyslav223/js_test' },
