ce3ddb32716943f3e0685e46b49e92706cea6fe4
Yurii Holiuk <bionicuss@gmail.com>
2020-03-31 18:48:32 +0300

    Updates

---
+++ precache-manifest.84ae960e975ec4679f829ba873a56b5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd89115de261cc11c337f53d358713d3",
+    "url": "/friendly-octo-pancake/index.html"
+  },
+  {
+    "revision": "c17a7035d77be784aba2",
+    "url": "/friendly-octo-pancake/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b2c5456bf3fea671b2af",
+    "url": "/friendly-octo-pancake/static/css/main.1afcf294.chunk.css"
+  },
+  {
+    "revision": "c17a7035d77be784aba2",
+    "url": "/friendly-octo-pancake/static/js/2.3bcbdedb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/friendly-octo-pancake/static/js/2.3bcbdedb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2c5456bf3fea671b2af",
+    "url": "/friendly-octo-pancake/static/js/main.7f8d5c6a.chunk.js"
+  },
+  {
+    "revision": "9d7f2bf8d9131076f13b",
+    "url": "/friendly-octo-pancake/static/js/runtime-main.ed7e1a3a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.d801bd14c75c16f9380c99b556b782aa.js"
+  "/friendly-octo-pancake/precache-manifest.84ae960e975ec4679f829ba873a56b5f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/friendly-octo-pancake/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
